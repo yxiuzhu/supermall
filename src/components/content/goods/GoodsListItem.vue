828f53fd9b8411c8c9e952de09c91d94f7cd7ff4
y_xiuzhu_glb@163.com <y_xiuzhu@163.com>
2020-11-14 14:42:14 +0800

    缺少样式列表的样式，可以用来练习样式

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="goodsItem.show.img" alt="">
+    <p>{{goodsItem.title}}</p>
+    <span class="price">{{goodsItem.price}}</span>
+    <span class="collect">{{goodsItem.cfav}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GoodsListItem',
+    props: {
+      goodsItem: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/network/home.js
+++ src/network/home.js
@@ -1,7 +1,18 @@
 import {request} from "./request";
 
-export function  getHomeMultidata() {
+export function getHomeMultidata() {
   return request({
     url: '/home/multidata'
+  })
+}
+
+// 请求商品详情页的数据
+export function getHomeGoods(type, page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page 
+    }
   })
 }
